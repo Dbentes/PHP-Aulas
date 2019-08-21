ad0f7cc7f7ddbb7dbdc57ad7afee048b95115248
Dbentes <dmrsn.diego@gmail.com>
2019-08-21 12:57:49 -0400

    Novos Projetos aulas PHP

---
+++ PhpstormProjects/aula04/tipos.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="css/estilo.css"/>
+    <title></title>
+</head>
+<body>
+<div>
+    <?php
+        $nome= Diego;
+        $idade = 26;
+        echo "$nome tem $idade anos!";   /*Pra escrever na tela e "Echo"*/
+    ?>
+</div>
+</body>
+</html>
